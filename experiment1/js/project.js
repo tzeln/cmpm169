8d26fb08b7981e9719a2b8387a39b32a27c826ce
tzeln <107380713+tzeln@users.noreply.github.com>
2024-01-23 20:23:25 -0800

    literally all of experiment 2

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Nick Tung  
+// Date: Jan 22, 2024
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
@@ -31,4 +31,4 @@ function main() {
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,67 +1,63 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - experiment 2 coded contents here 
+// Author: Nick 
+// Date: Jan 22, 2024
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
 let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
 
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+    background(20, 20, 100);
     noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    let gridSize = 50;
+    let boxSize = 50;
+    let totalSize = gridSize * boxSize;
+    translate(-totalSize / 2, -totalSize / 2); 
+  
+    for (let y = 0; y < gridSize; y++) {
+      for (let x = 0; x < gridSize; x++) {
+        push();
+        translate(x * boxSize, y * boxSize);
+        let rotationX = map(mouseY, 0, height, -PI, PI);
+        let rotationY = map(mouseX, 0, width, -PI, PI);
+  
+        rotateX(rotationX);
+        rotateY(rotationY);
+        genBox(x * boxSize, y * boxSize);
+        pop();
+      }
+    }
 }
+
+function genBox(x, y) {
+    let rainbowColor = getRGB(x, y);
+    fill(rainbowColor);
+    for (let i = 0; i < 10; i+=3) {
+      rotateX(frameCount * 0.01+i);
+      rotateY(frameCount * 0.01+i);
+    }
+    box(30);
+  }
+
+  function getRGB(x, y) {
+    let hue = map(x + y, 0, 45, 0, 360); 
+    hue = hue % 360; 
+    return color('hsb(' + hue + ', 80%, 100%)');
+  }
 
 // mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+// function mousePressed() {
+//     code to run when mouse is pressed
+// }
