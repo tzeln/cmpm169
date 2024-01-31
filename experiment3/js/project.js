ff96e22b523f963af7cf4b32c5de4e8c35f9813d
tzeln <107380713+tzeln@users.noreply.github.com>
2024-01-30 21:16:28 -0800

    adding experiment 3

--- experiment3/js/project.js
+++ experiment3/js/project.js
@@ -1,34 +1,75 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Nick Tung
+// Date:  1/29/24
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
+//mouse-gnereated ripple logic created almost entirely from ChatGPT
+let ripples = [];
+let speed = 0; 
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(255);
+  // background(30, 70, 105);
+
+  if (mouseIsPressed) {
+    let mouseRipple = new Ripple(mouseX, mouseY);
+    mouseRipple.alpha = 0; 
+    ripples.push(mouseRipple);
   }
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+  let x = 300 + sin(speed)*100;
+  let y = 300 + cos(speed)*100;
+  let genRipple = new Ripple(x, y);
+  ripples.push(genRipple);
+    
+
+  
+  speed += 1;
+  let g = 50;
+  for (let i = ripples.length - 1; i >= 0; i--) {
+    ripples[i].update();
+    ripples[i].display(g);
+    if (ripples[i].isFinished()) {
+      ripples.splice(i, 1); 
+      ripples.splice(i, 2);
+      let newRipple = new Ripple(random(width), random(height));
+      ripples.push(newRipple);
+    }
+    g += 5;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass(value1, value2);
-
-  // call a method on the instance
-  myInstance.myMethod();
+class Ripple {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 0;
+    this.maxRadius = 200; 
+    this.expandRate = 3; 
+    this.alpha = 255; 
+  }
+  
+  update() {
+    this.radius += this.expandRate;
+    this.alpha -= 5;
+  }
+  
+  display(x) {
+    noFill();
+    stroke(30, x, 160, this.alpha);
+    ellipse(this.x, this.y, this.radius * 2);
+  }
+  
+  isFinished() {
+    return this.radius >= this.maxRadius || this.alpha <= 0;
+  }
 }
 
-// let's get this party started - uncomment me
-//main();
