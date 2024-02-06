96eb45582678cb42037c6e38658032b1c0888d43
tzeln <107380713+tzeln@users.noreply.github.com>
2024-02-05 22:13:55 -0800

    adding everything so far

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,31 +1,16 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Experiment 4
+// Author: Nick Tung
+// Date: 2/5/24
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
+let img;
+let sound;
 let canvasContainer;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+    img = loadImage('data/uni_the_strongest.jpg');
+    sound = loadSound('data/cat_meow.mp3');
 }
 
-// setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
@@ -34,34 +19,30 @@ function setup() {
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+        resizeCanvas(img.width(), img.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    image(img, 0, 0);
 }
-
-// draw() function is called repeatedly, it's the main animation loop
+  
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+    background(255);
     noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    for (let y = 0; y < img.height; y += 5) { 
+        for (let x = 0; x < img.width; x += 5) { 
+            let pixel = img.get(x, y);
+            let greyscale = round(red(pixel) * 0.222 + green(pixel) * 0.707 + blue(pixel) * 0.071);
+            let thickness = map(greyscale, 0, 255, 20, 1); 
+            textSize(thickness);
+            if (thickness < 10) {
+                text(':>', x, y);
+            } else {
+                text(':3', x, y);
+            }
+        }    
+    }
+    frameRate(60);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+    sound.play();
 }
