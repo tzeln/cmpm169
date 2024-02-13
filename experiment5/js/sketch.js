577c3419a3b6b78a21b6ba24cd71e06c30279a76
tzeln <107380713+tzeln@users.noreply.github.com>
2024-02-12 22:48:36 -0800

    adding all of exp 5

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,67 +1,58 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Nick Tung    
+// Date: 2/12/24
+// beginning reference (was initially a 1-1 copy!): https://www.youtube.com/watch?v=R1tfyVyU0hg&list=PLwUlLzAS3RYow0T9ZXB0IomwB-DyBRTfm&index=6
+// changes made: custom canvas sized, altered certain function parameters (like RGB), mouse integration corresponds to viewing angle as well as perceived perlin strength (I think)
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let isMouseOver = false;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
-    // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+        resizeCanvas(canvasContainer.width, canvasContainer.height);
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    angleMode(DEGREES);
+    noiseDetail(1);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    background(0);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
+    // moves the scene back along the z-axis and rotates for visibility
+    translate(0, 0, -300);
+    let rotationX = map(mouseY, 0, height, 0, 360);
+    let rotationY = map(mouseX, 0, width, 0, 360);
+    rotateX(rotationX);
+    rotateY(rotationY);
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+    var w = 30; // w stores the size (width) of each box in the scene
+    var start = frameCount / 100; // used to animate noise pattern; changes slightly based on each incr frame
+    var xoff = 0; // used as the X offset for the perlin noise function
+
+    // iterates over the width of the canvas
+    for (var x = -700; x <= 700; x+=w) {
+        yoff = 0; // sets Y offset for the perlin noise function
+        
+        // iterates over the height of the canvas
+        for (var y = -700; y <= 700; y+=w) {
+            var h = map(noise(xoff + start, yoff + start)*2, 0, 1, -200, 0); // uses Perlin noise and xoff + yoff to generate a height value between -100 and 100
+    
+            // creates gradient based on x, y, and h (height)
+            // translates current box to position and draws given found specifications w, w, h
+            var r = map(x, -width/2, width/2, 0, 255);
+            var g = map(y, -height/2, height/2, 255, 0);
+            var b = map(h, -200, 0, 0, 255);
+            push();
+            fill(r, g, b);
+            translate(x, y, map(mouseX, 0, h, -200, 0));
+            // translate(x, y, -h/2);
+            box(w, w, h);
+            pop()
+            yoff += 0.06;
+        }
+        xoff += 0.06;
+    }
 }
